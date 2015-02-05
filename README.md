0f634d9594c25f8a8f9465b5e82beb6de4fe9dc8
accusedSaturn <kaylathewizard@gmail.com>
2015-02-05 20:07:14 +0430

    Adds: fading quote purple background

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Slideshow
+
+This is a simple slideshow done with HTML, CSS and Javascript.
---
+++ js/main.js
@@ -0,0 +1,4 @@
+$(function(){
+	$('#the-quote').html('I\'m selfish, impatient, and a little insecure. I make mistakes, I\'m out of control, and at times hard to handle. But if you can\'t handle me at my worst, then you sure as hell don\'t deserve me at my best.');
+	$('.big-quote').fadeIn(4000);
+});
